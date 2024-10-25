57f05497f7b011fddcf4c7de1fb71f2a0bbeae93
Ramos-EA4 <leeamramos@gmail.com>
2024-10-25 11:39:09 +0800

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -9,17 +10,90 @@ void main() {
           title: const Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: const DicePage(),
+        body: DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({super.key});
 
   @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int left = 1, right = 1;
+  String text = 'Roll the dice';
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            text,
+            style: const TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        ChangeValue();
+                      });
+                      print('Left image is clicked');
+                      print(left);
+                    },
+                    child: Image.asset('images/dice$left.png'),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        ChangeValue();
+                      });
+                      print('Right image is clicked');
+                    },
+                    child: Image.asset('images/dice$right.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void ChangeValue() {
+    setState(() {
+      left = 1 + Random().nextInt(6);
+      right = 1 + Random().nextInt(6);
+      result();
+    });
+  }
+
+  void result() {
+    if (left > right) {
+      text = 'Left Wins';
+    } else if (left < right) {
+      text = 'Right Wins';
+    } else {
+      text = 'Draw';
+    }
   }
 }
